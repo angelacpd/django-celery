ff6bd71a471e27c08bfb74ff04aadb9c605279db
Angela Crepaldi <angelacrepaldi@gmail.com>
2022-01-09 20:31:40 -0300

    Downgrade django to install django-celery-beat. Install django-celery-beat to run periodic tasks and flower to monitor tasks.

--- core/settings.py
+++ core/settings.py
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party
+    'django_celery_beat',
+
     # Local
     'app1.apps.App1Config',
     'app2.apps.App2Config',
+    'app3.apps.App3Config',
 ]
 
 MIDDLEWARE = [
